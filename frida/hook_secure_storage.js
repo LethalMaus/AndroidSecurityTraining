e2fc9b88a48f6575bd017fed4b592fc55b209f42
LethalMaus <descriptiveanimals@gmail.com>
2025-11-18 21:25:13 +0100

    Fixed example frida script

--- frida/hook_secure_storage.js
+++ frida/hook_secure_storage.js
@@ -1,19 +1,17 @@
 Java.perform(() => {
   const H = Java.use('dev.jamescullimore.android_security_training.storage.SecureStorageHelper');
 
-  // Pick the exact overload explicitly (good habit)
   const saveTokenOverload = H.saveTokenSecure.overload(
     'android.content.Context',
     'java.lang.String',
-    'w1.c'
+    'kotlin.coroutines.Continuation'
   );
 
-  saveTokenOverload.implementation = function (ctx, token, extra) {
+  saveTokenOverload.implementation = function (ctx, token, cont) {
     console.log('[Frida] saveTokenSecure token =', token);
-    console.log('[Frida] saveTokenSecure extra =', extra);
 
-    // Always call original with ALL args
-    return saveTokenOverload.call(this, ctx, token, extra);
-    // or: return this.saveTokenSecure(ctx, token, extra);
+    const result = saveTokenOverload.call(this, ctx, token, cont);
+    console.log('[Frida] original returned:', result);
+    return result;
   };
 });
